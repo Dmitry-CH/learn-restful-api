02bb19d3eacede0663baf4510bfbd45cdae4d4c6
Dmitry Chekanov <chd@kodix.ru>
2022-12-09 14:04:35 +0300

    Init project

---
+++ README.md
@@ -0,0 +1,11 @@
+# Учебный проект по разработке RESTful API на Clojure
+
+:warning: Внимание, приложение находится в разработке :warning:
+
+## Usage
+
+Для разработки потребуется установить на вашу машину [leiningen](https://leiningen.org/), а также доп. инструменты [calva](https://calva.io/).
+
+* запустить локально (dev)
+
+        $ lein with-profile dev run
---
+++ project.clj
@@ -0,0 +1,43 @@
+(defproject restful "0.1.0-SNAPSHOT"
+
+  :description ""
+  :url ""
+
+  :dependencies [[com.taoensso/timbre "6.0.4"]
+                 [cprop "0.1.19"] 
+                 [mount "0.1.16"]
+                 [nrepl "1.0.0"]
+                 [org.clojure/clojure "1.11.1"]
+                 [org.clojure/tools.cli "1.0.214"]
+                 [ring/ring-jetty-adapter "1.9.6"]]
+
+  :min-lein-version "2.0.0"
+
+  :main ^:skip-aot restful.core
+
+  :source-paths ["src/clj"]
+  :resource-paths ["resources"]
+  :target-path "target/%s/" 
+
+  :plugins [[lein-pprint "1.3.2"]]
+
+  :profiles
+  {:uberjar {:aot :all
+             :omit-source true
+             :source-paths ["env/prod/clj"]
+             :resource-paths ["env/prod/resources"]
+             :uberjar-name "restful.jar"}
+   
+   :dev [:project/dev :profiles/dev]
+   
+   :project/dev {:dependencies [[org.clojure/tools.namespace "1.3.0"]]
+                 
+                 :source-paths ["env/dev/clj"]
+                 :resource-paths ["env/dev/resources"]
+                 
+                 :plugins [[jonase/eastwood "1.3.0"]]
+                 
+                 :repl-options {:init-ns user
+                                :timeout 120000}}
+   
+   :profiles/dev {}})
